b05cf5b475315056e5a4ba0902ebee40a887e765
BillJesser <127352761+BillJesser@users.noreply.github.com>
2024-05-27 16:49:22 -0400

    Update App.js

--- Front-End/cash-crop/App.js
+++ Front-End/cash-crop/App.js
@@ -3,18 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './pages/login';
 import RegisterScreen from './pages/register';
-
+import DashboardScreen from './pages/userDashboard';
 
 const Stack = createStackNavigator();
 
-
 export default function App() {
- return (
- <NavigationContainer>
- <Stack.Navigator initialRouteName="Home">
- <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Cash Crop' }} />
- <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Register' }} />
- </Stack.Navigator>
- </NavigationContainer>
- );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Cash Crop' }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Register' }} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ title: 'Dashboard' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
