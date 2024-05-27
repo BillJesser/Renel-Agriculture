7c320bb0b8fc886b63078bafdb88197a485a429d
JuanCarlos200321 <juancarlosplate@ufl.edu>
2024-05-27 12:30:05 +0000

    User functions and app organization

---
+++ Back-End/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+#create app session
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+@cross_origin
+def home():
+    return
+
+
+
+
+#start app
+if __name__ == "__main__":
+    cors = CORS(app)
+    app.run()
---
+++ Back-End/users.py
@@ -0,0 +1,59 @@
+import pymongo as pym
+import bcrypt
+
+client = pym.MongoClient("mongodb://localhost:27017/")
+
+db = client["cashcrop"]
+users = db["users"]
+
+def hash_function(password):
+    s = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), s)
+    return hashed_password
+
+def register_user(username, phone, birthdate):
+
+    if users.find_one({"username": username, "Date of Birth": birthdate}):
+        return False
+
+    # Hash the password before storing it
+    hashed_password = hash_function(phone)
+
+    # Insert the new user into the users collection with hashed password
+    user_data = {
+        "username": username,
+        "password": hashed_password,
+        "Date of Birth": birthdate,
+        "user_type": "Client",
+    }
+    users.insert_one(user_data)
+    return True
+
+def login(username, password, birthdate):
+    # Search database for user
+    user = users.find_one({"username": username, "Date of Birth": birthdate})
+    # Check if username and password match
+    if user:
+        hashpass = user["password"]
+        return bcrypt.checkpw(password.encode("utf-8"), hashpass)
+    else:
+        return False
+    
+def delete_user(username, password):
+
+    # Find username
+    user = users.find_one({"username": username})
+
+    if user:
+        hashpass = user["password"]
+        if bcrypt.checkpw(password.encode("utf-8"), hashpass):
+            # Delete the user
+            users.delete_one({"_id": user["_id"]})
+            return True
+
+    return False 
+
+
+if __name__ == "__main__":
+   yo = delete_user("Kofi", "9549092733")
+   print(yo)
